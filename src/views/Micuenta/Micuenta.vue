7205a2bda6d785c5af180e37d9c8786f5a8d63da
Ezequiel <nikocor99@gmail.com>
2018-09-04 20:31:05 +0200

    Cambios a MiCuenta

--- src/views/Micuenta/Micuenta.vue
+++ src/views/Micuenta/Micuenta.vue
@@ -3,22 +3,42 @@
 <!--La etiqueta TEMPLATE sólo puede tener un hijo n este caso el DIV con ID = "HomeContainer"-->
 <template>
   <!--Cada página debería tener este div, con la misma clase pero distinto ID-->
-  <div id="MicuentaContainer" class="pageContainer">
+  <div id="MiCuentaContainer" class="pageContainer">
     <!--Este es el encabezado de la página-->
     <header id="appHeader">
       <!--Este es el botón hamburguesa que abre el menu lateral-->
       <button id="hamburguer" class="button float-left" @click="toggleNav()"><i class="mdi mdi-menu"></i></button>
       <!--Este es el título de la página-->
-      <h1 class="float-left">Libros Libres</h1>
+      <h1 class="float-left">Editar Perfil</h1>
       <!--Este es el botón de la derecha-->
       <button id="search" class="button float-right"><i class="mdi mdi-magnify"></i></button>
       <div class="clr"></div>
     </header>
-            <div class="clr"></div>
-        <div id="MicuentaContent"> 
-            <h1><img src="../../assets/prod162779_IMGSET.jpg"> </h1>
+    <div class="clr"></div>
+    <div class="MicuentaContent">
+     
+      <div class="imagenperfil">
+
+        <img src="../../assets/prod162779_IMGSET.jpg">
+
       </div>
-      </div>
+      
+     <div class="Infouser">
+        
+      <h2>Nombre</h2>
+      <input type="text">
+
+      <h2>Email</h2>
+      <input type="text">
+
+      <h2>Generos Favoritos</h2>
+      <input type="text">
+    
+     </div> 
+     
+    
+    </div>
+  </div>
 </template>
 
 <!--Cada página tiene su sección SCRIPT-->
@@ -37,5 +57,5 @@ export default {
 
 <!--En esta sección importamos los estilos de esta página-->
 <style scoped>
-/*@import "home.min.css"; /*Los archivos SCSS nos permiten escribir CSS automatizado*/
+@import "micuenta.css"; /*Los archivos SCSS nos permiten escribir CSS automatizado*/
 </style>
